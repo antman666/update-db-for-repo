8088480fdb5f7b1875fd09a6b0466a7c8f91fe44
antman666 <945360554@qq.com>
2021-08-13 11:35:50 +0800

    update something

--- gendb.sh
+++ gendb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir aya -p
+mkdir kernels -p
 
-urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/test/releases/tag/x86_64 -s|grep "/antman666/test/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
-pkg=$(echo $i|awk -F '/' '{print$7}')
-echo Downloading $pkg
-wget -q https://github.com/$i -P aya/
-repo-add aya.db.tar.gz ./aya/$pkg
+		pkg=$(echo $i|awk -F '/' '{print$7}')
+		echo Downloading $pkg
+		wget -q https://github.com/$i -P kernels/
+		repo-add kernels.db.tar.gz ./kernels/$pkg
 done
 
