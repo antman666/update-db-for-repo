837f6ce1577918bd5a5dd15480430fe7a5219e05
antman666 <945360554@qq.com>
2021-09-19 22:52:39 +0800

    update something

--- gendb.sh
+++ gendb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir kernels -p
+mkdir own-kernels -p
 
-urls=$(curl https://github.com/antman666/kernel-repo/releases/tag/x86_64 -s|grep "/antman666/kernel-repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/own_kernel/releases/tag/x86_64 -s|grep "/antman666/own_kernel/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i|awk -F '/' '{print$7}')
 		echo Downloading $pkg
-		wget -q https://github.com/$i -P kernels/
-		repo-add -p kernels.db.tar.gz ./kernels/$pkg
+		wget -q https://github.com/$i -P own-kernels/
+		repo-add -p own-kernels.db.tar.gz ./own-kernels/$pkg
 done
 
