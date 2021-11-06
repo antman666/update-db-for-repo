1b0aa3bb712ea6bebc4961ff367c81470f589a4a
antman666 <945360554@qq.com>
2021-11-06 15:19:24 +0800

    update repo

--- gendb.sh
+++ gendb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir own-kernels -p
+mkdir own-repo -p
 
-urls=$(curl https://github.com/antman666/own_kernel/releases/tag/x86_64 -s|grep "/antman666/own_kernel/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/own_repo/releases/tag/x86_64 -s|grep "/antman666/own_repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i|awk -F '/' '{print$7}')
 		echo Downloading $pkg
-		wget -q https://github.com/$i -P own-kernels/
-		repo-add -p own-kernels.db.tar.gz ./own-kernels/$pkg
+		wget -q https://github.com/$i -P own-repo/
+		repo-add -p own-repo.db.tar.gz ./own-repo/$pkg
 done
 
