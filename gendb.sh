abdb87a9173549c36792d464849cf4d15c4942b0
antman666 <945360554@qq.com>
2022-02-08 10:40:46 +0800

    update repo

--- gendb.sh
+++ gendb.sh
@@ -8,6 +8,6 @@ for i in ${urls[*]}; do
 		pkg=$(echo $i|awk -F '/' '{print$7}')
 		echo Downloading $pkg
 		wget -q https://github.com/$i -P own-repo/
-		repo-add -p own-repo.db.tar.gz ./own-repo/$pkg
 done
 
+repo-add -p own-repo.db.tar.gz ./own-repo/*.zst
