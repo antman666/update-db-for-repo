9bea4d6fff19a169df5b816888ca67e6781a9963
antman666 <945360554@qq.com>
2022-07-03 10:31:30 +0800

    update repo

--- gendb.sh
+++ gendb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mkdir aya -p
+mkdir action -p
 
-urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/aya"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman/action-tool/releases/tag/x86_64 -s|grep "/antman/action-tool/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
-db=$(echo $i|awk -F '/' '{print$7}')
-echo Downloading $db
-wget -q https://github.com/$i
-repo-add aya.db.tar.gz ./*/*.pkg.tar.zst
+  db=$(echo $i|awk -F '/' '{print$7}')
+  echo Downloading $db
+  wget -q https://github.com/$i -P action
 done
+
+repo-add -p action.db.tar.gz ./action/*.zst
