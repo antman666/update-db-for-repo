495371078a561267279d7fca536a7dcea753e9d1
antman666 <945360554@qq.com>
2022-09-18 11:26:36 +0800

    fix update db

--- gendb.sh
+++ gendb.sh
@@ -2,11 +2,11 @@
 
 mkdir kernels -p
 
-urls=$(curl https://github.com/antman666/kernel-repo/releases/tag/x86_64 -s|grep "/antman666/kernel-repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+urls=$(curl "https://api.github.com/repos/antman666/kernel-repo/releases/latest" -s | grep browser_download_url | awk '{print $2}')
 
 for i in ${urls[*]}; do 
-		pkg=$(echo $i|awk -F '/' '{print$7}')
-		echo Downloading $pkg
+		pkg=$(echo $i|awk -F '/' '{print$9}')
+		echo Downloading ${pkg%?}
 		wget -q https://github.com/$i -P kernels/
 done
 
