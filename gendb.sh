a739ba54c18ae6257346732d697a1fd3ef22118f
antman666 <945360554@qq.com>
2022-09-18 11:46:12 +0800

    fix update db

--- gendb.sh
+++ gendb.sh
@@ -5,9 +5,9 @@ mkdir kernels -p
 urls=$(curl "https://api.github.com/repos/antman666/kernel-repo/releases/latest" -s | grep browser_download_url | awk '{print $2}')
 
 for i in ${urls[*]}; do 
-		pkg=$(echo $i|awk -F '/' '{print$9}')
+		pkg=$(echo $i | awk -F '/' '{print$9}')
 		echo Downloading ${pkg%?}
-		wget -q https://github.com/$i -P kernels/
+		wget -q ${i//\"/} -P kernels/
 done
 
 repo-add -p kernels.db.tar.gz ./kernels/*.zst
