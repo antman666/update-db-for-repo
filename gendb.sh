8c4e9379b115b9ab2f189bbc7c17e3adf1fe0571
antman666 <945360554@qq.com>
2022-10-15 22:15:55 +0800

    update repo

--- gendb.sh
+++ gendb.sh
@@ -5,7 +5,7 @@ mkdir action-repo -p
 urls=$(curl "https://api.github.com/repos/antman666/action-repo/releases/latest" -s | grep browser_download_url | awk '{print $2}')
 
 for i in ${urls[*]}; do 
-  db=$(echo $i|awk -F '/' '{print$7}')
+  db=$(echo $i|awk -F '/' '{print$9}')
   echo Downloading $db
   wget -q https://github.com/$i -P action-repo
 done
