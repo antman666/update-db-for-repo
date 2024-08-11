06385e675fabf0169ce58ade403d8400865a0655
antman666 <62091572+antman666@users.noreply.github.com>
2024-08-11 23:16:33 +0800

    Update gendb.sh

--- gendb.sh
+++ gendb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir kernels -p
+mkdir cosmic -p
 
-urls=$(curl "https://api.github.com/repos/antman666/kernel-repo/releases/latest" -s | grep browser_download_url | awk '{print $2}')
+urls=$(curl "https://api.github.com/repos/antman666/cosmic-repo/releases/latest" -s | grep browser_download_url | awk '{print $2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i | awk -F '/' '{print$9}')
 		echo Downloading ${pkg%?}
-		wget -q ${i//\"/} -P kernels/
+		wget -q ${i//\"/} -P cosmic/
 done
 
-repo-add -p kernels.db.tar.gz ./kernels/*.zst
+repo-add -p cosmic.db.tar.gz ./cosmic/*.zst
